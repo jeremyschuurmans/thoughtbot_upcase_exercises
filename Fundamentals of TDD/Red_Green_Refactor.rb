d1e55e435eb1e3df12f44e4c200d2253a49fefd6
Jeremy Schuurmans <schuurm@ns.codes>
2019-06-05 18:11:36 -0700

    slight changes to variables in initialize method. Start first test

--- Fundamentals of TDD/Red_Green_Refactor.rb
+++ Fundamentals of TDD/Red_Green_Refactor.rb
@@ -16,7 +16,7 @@ require "rspec/autorun"
 # create the descriptions to match the requirements above.
 
 class Person
-  def initialize(first_name:, middle_name: nil, last_name:)
+  def initialize(first_name, middle_name = nil, last_name)
     @first_name = first_name
     @middle_name = middle_name
     @last_name = last_name
@@ -27,7 +27,10 @@ end
 
 RSpec.describe Person do
   describe "#full_name" do
-    it "concatenates first name, middle name, and last name with spaces"
+    it "concatenates first name, middle name, and last name with spaces" do
+      jeremy = Person.new("Jeremy", "Martin", "Schuurmans")
+      
+    end
     it "does not add extra spaces if middle name is missing"
   end
 
