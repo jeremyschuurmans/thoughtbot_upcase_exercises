6c3f3543d2045aaf6f7e5a211c2ac2d1e6923a63
Jeremy Schuurmans <schuurm@ns.codes>
2019-06-05 18:37:21 -0700

    refactor using string interpolation

--- Fundamentals of TDD/Red_Green_Refactor.rb
+++ Fundamentals of TDD/Red_Green_Refactor.rb
@@ -23,7 +23,7 @@ class Person
   end
 
   def full_name
-    @middle_name == "" ? @first_name + " " + @last_name : @first_name + " " + @middle_name + " " + @last_name
+    @middle_name == "" ? "#{@first_name} #{@last_name}" : "#{@first_name} #{@middle_name} #{@last_name}"
   end
 
   def full_name_with_middle_initial
